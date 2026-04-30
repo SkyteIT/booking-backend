c43daf17148ccda5db235ebd568d17a4fea7917f
Isuru Kavinda <isurukpremasiri2001@gmail.com>
2026-04-30 13:06:47 +0530

    implement Application Controller

---
+++ src/Ube.Api/Controllers/Admin/VendorApplicationController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Ube.Application.Features.Admin.VendorApplications;
+using Ube.Application.Features.Vendors;
+using Ube.Application.Common.Interfaces.Services.Auth;
+
+namespace Ube.Api.Controllers.Admin;
+
+[Authorize(Roles = "Admin")]
+[ApiController]
+[Route("api/admin/vendor-applications")]
+public class VendorApplicationsController : ControllerBase
+{
+    private readonly IAdminVendorApplicationService _service;
+    private readonly ICurrentUserService _currentUser;
+
+    public VendorApplicationsController(
+        IAdminVendorApplicationService service,
+        ICurrentUserService currentUser)
+    {
+        _service = service;
+        _currentUser = currentUser;
+    }
+
+    //   (Approve / Reject)
+    [HttpPatch("{applicationId}/review")]
+    public async Task<IActionResult> ReviewApplication(
+        Guid applicationId,
+        [FromBody] ReviewVendorApplicationDto dto)
+    {
+        var userId = _currentUser.UserId;
+        await _service.ReviewApplicationAsync(
+            applicationId,
+            userId, // adminId from JWT
+            dto
+        );
+
+        return Ok(new { message = "Application reviewed successfully" });
+    }
+    [HttpGet("{applicationId}")]
+    public async Task<IActionResult> GetDetails(Guid applicationId)
+    {
+        var result = await _service.GetDetailsAsync(applicationId);
+        return Ok(result);
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _service.GetAllAsync();
+        return Ok(result);
+    }
+}

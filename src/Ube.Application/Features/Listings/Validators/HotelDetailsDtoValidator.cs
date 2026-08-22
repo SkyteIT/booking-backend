77a48e9d3c59d99551308cee50629490caed56dd
NathashaOshani <nathashaoshani9@gmail.com>
2026-08-22 17:46:11 +0530

    feat: improve listing details and validation

--- src/Ube.Application/Features/Listings/Validators/HotelDetailsDtoValidator.cs
+++ src/Ube.Application/Features/Listings/Validators/HotelDetailsDtoValidator.cs
@@ -13,10 +13,14 @@ public class HotelDetailsDtoValidator : AbstractValidator<HotelDetailsDto>
             .GreaterThanOrEqualTo(0).WithMessage("Available rooms cannot be negative");
 
         RuleFor(x => x.CheckInTime)
-            .NotEmpty().WithMessage("Check-in time is required");
+            .NotEmpty().WithMessage("Check-in time is required")
+            .Matches(ListingTimeFormat.TimePattern)
+            .WithMessage("Check-in time must use 12-hour AM/PM format, for example 2:00 PM");
 
         RuleFor(x => x.CheckOutTime)
-            .NotEmpty().WithMessage("Check-out time is required");
+            .NotEmpty().WithMessage("Check-out time is required")
+            .Matches(ListingTimeFormat.TimePattern)
+            .WithMessage("Check-out time must use 12-hour AM/PM format, for example 11:00 AM");
 
         RuleFor(x => x.Amenities)
             .NotEmpty().WithMessage("At least one amenity is required");
